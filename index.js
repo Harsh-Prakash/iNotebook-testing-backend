1a857e8dadcab640bbf19366a9faeb203dc68a27
Harsh-Prakash <138960290+Harsh-Prakash@users.noreply.github.com>
2024-02-25 19:06:21 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
 app.listen(process.env.PORT, () => {
-  console.log(`iNoteBook Backend listening on port ${port}`)
+  console.log(`iNoteBook Backend listening on port ${process.env.PORT}`)
 })
 connectToMongo();
